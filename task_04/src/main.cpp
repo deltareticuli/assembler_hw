91984eeb510df6a9fe70198dbf9786ccf23f912f
Ivan <deltareticuli@gmail.com>
2020-11-30 18:42:41 +0300

    Task 4 updated

--- task_04/src/main.cpp
+++ task_04/src/main.cpp
@@ -3,20 +3,31 @@
 #include <functional>
 #include <omp.h>
 
-const char USAGE_INFO[] = "Usage: <path to executable file> <array size>";
+const char USAGE_INFO[] = "Usage: <path to executable file> <monastery size>";
 
-int find_max(int *array, int size) {
-    int v1, v2, res;
+class Monk {
+public:
+    int energy{0};
+    int monastery{0};
+    Monk() {}
+    explicit Monk(int i, int m) : energy(i), monastery(m) {}
+};
+
+Monk find_max(Monk *array, int size) {
+    Monk m1, m2, res;
     if (size == 1) return array[0];
     int mid = size % 2 == 0 ? size / 2 : size / 2 + 1;
-#pragma omp task shared(v1)
-    v1 = find_max(array, mid);
-#pragma omp task shared(v2)
-    v2 = find_max(array + mid, mid);
+#pragma omp task shared(m1)
+    m1 = find_max(array, mid);
+#pragma omp task shared(m2)
+    m2 = find_max(array + mid, mid);
 #pragma omp taskwait 
 {
-    res = v1 >= v2 ? v1 : v2;
-    std::cout << "[Thread " << omp_get_thread_num() << "] Monk (" << v1 << ") is fighting against Monk (" << v2 << "). The winner is " << "Monk (" << res << ")" << std::endl; 
+    res = m1.energy >= m2.energy ? m1 : m2;
+    std::cout << "[Thread " << omp_get_thread_num() << "]";
+    std::cout << " Monk (" << m1.energy << ") from " << (m1.monastery == 0 ? "Guan-Yin" : "Guan-Yang");
+    std::cout << " is fighting against Monk (" << m2.energy << ") from " << (m2.monastery == 0 ? "Guan-Yin" : "Guan-Yang");
+    std::cout << ". The winner is Monk (" << res.energy << ") from " << (res.monastery == 0 ? "Guan-Yin." : "Guan-Yang.") << std::endl;
     return res;
 }
 }
@@ -25,6 +36,7 @@ int find_max(int *array, int size) {
 int main(int argc, char *argv[]) {
     uint32_t size;
     int thread_number = omp_get_max_threads();
+    srand(time(0));
 
     if (argc != 2) {
         std::cout << USAGE_INFO << std::endl;
@@ -38,15 +50,15 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    int *values = new int[size];
+    Monk *values = new Monk[size*2];
 
-    for (int i = 0; i < size; i++) {
-        values[i] = i + 1;
+    for (int i = 0; i < size*2; i++) {
+        values[i] = Monk(0 + rand() % 100, i % 2);
     }
 
-    int thread_results[thread_number];
+    Monk thread_results[thread_number];
 
-    uint32_t chunk_size = size / thread_number;
+    uint32_t chunk_size = size * 2 / thread_number;
 
 #pragma omp parallel
 {
@@ -58,6 +70,6 @@ int main(int argc, char *argv[]) {
     }
 }
 
-    int winner = find_max(thread_results, thread_number);
-    std::cout << "Monk (" << winner << ") has won the battle!" << std::endl;
+    Monk winner = find_max(thread_results, thread_number);
+    std::cout << (winner.monastery == 0 ? "Guan-Yin" : "Guan-Yang") << " has won the battle!" << std::endl;
 }
